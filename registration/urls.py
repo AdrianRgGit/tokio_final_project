23eead4332da50b9aead4e08a2326b0616faafbd
Adrián Ramírez <adrianramirezgalera@gmail.com>
2023-12-29 18:50:36 +0100

    pasado a clases

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import SignUpView, ProfileView
 from . import views
 
 urlpatterns = [
+    # El login lo traigo directamente de django
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path("signup/", views.signup, name="signup"),
-    path("profile/", views.profile, name="profile"),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,23 +1,32 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.views import View
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
-def signup(request):
-    if request.method == 'POST':
+class SignUpView(View):
+    template_name = 'registration/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
-    else:
-        form = UserCreationForm()
 
-    return render(request, 'registration/signup.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
 
-@login_required
-def profile(request):
-    return render(request, "registration/profile.html")
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    template_name = 'registration/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
