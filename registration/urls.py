009970a53483429a20d63378fedbf59ad867dcd1
Adrián Ramírez <adrianramirezgalera@gmail.com>
2023-12-29 18:43:34 +0100

    ya van los registros y los inicios de sesión

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("login/", views.login, name="login"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path("signup/", views.signup, name="signup"),
     path("profile/", views.profile, name="profile"),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
-def login(request):
-    return render(request, "registration/login.html")
-
-
 def signup(request):
-    return render(request, "registration/signup.html")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 @login_required
