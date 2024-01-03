443b0f61bba43ffdd0640d600184b9cac989448f
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-01-03 17:48:30 +0100

    falta episodios y solucionar url

--- content/urls.py
+++ content/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # Paths core
     path("movie/<int:pk>/", MovieDetailView.as_view(), name="movie_detail"),
     path("movie/create/", MovieCreateView.as_view(), name="create_movie"),
     path('movie/update/<int:pk>/', MovieUpdateView.as_view(), name="update_movie"),
     path('movie/delete/<int:pk>/', MovieDeleteView.as_view(), name="delete_movie"),
+
     path("serie/<int:pk>/", SerieDetailView.as_view(), name="serie_detail"),
+
+    path("serie/<int:pk>/seasons/", SeasonListView.as_view(), name="season_list"),
+    path("serie/<int:serie_pk>/seasons/<int:season_pk>/", SeasonDetailView.as_view(), name="season_detail"),
+
+    path("serie/<int:serie_pk>/seasons/<int:season_pk>/episodes", EpisodeListView.as_view(), name="episode_list"),
+
     path("favorite_movies/", FavoriteMoviesView.as_view(), name="favorite_movies"),
 ]
--- content/views.py
+++ content/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 from django.views.generic.base import View
 from django.urls import reverse_lazy
 
-from .models import Movie, Serie
+from .models import Movie, Serie, Season, Episode
 
 from .forms import MovieForm
 
 
+@method_decorator(login_required, name='dispatch')
 class MovieDetailView(DetailView):
     template_name = "content/movie_detail.html"
     model = Movie
@@ -39,6 +40,68 @@ class MovieDeleteView(DeleteView):
 class SerieDetailView(DetailView):
     template_name = "content/serie_detail.html"
     model = Serie
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number_seasons'] = Season.objects.filter(serie_id=self.object).count()
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class SeasonListView(ListView):
+    model = Season
+    template_name = "content/season_list.html"
+
+    def get_queryset(self):
+        return Season.objects.filter(serie_id=self.kwargs['pk'])
+
+    # Para cambiar el nombre de object_list a seasons
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['seasons'] = context.pop('object_list')
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class SeasonDetailView(DetailView):
+    template_name = "content/season_detail.html"
+    model = Season
+
+    # Para las rutas que necesitan más de 1 pk
+    def get_object(self):
+        return get_object_or_404(Season, serie_id=self.kwargs['serie_pk'], pk=self.kwargs['season_pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number_episodes'] = Episode.objects.filter(season_id=self.object).count()
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class EpisodeListView(ListView):
+    template_name = "content/episode_list.html"
+    model = Episode
+
+    def get_queryset(self):
+        return Episode.objects.filter(season_id=self.kwargs['season_pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['episodes'] = context.pop('object_list')
+        return context
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(Episode, season_id=self.kwargs['season_pk'], serie_id=self.kwargs['serie_pk'])
+
+
+@method_decorator(login_required, name='dispatch')
+class EpisodeDetailView(DetailView):
+    template_name = "content/episode_detail.html"
+    model = Episode
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(Episode, season_id=self.kwargs['season_pk'], serie_id=self.kwargs['serie_pk'],
+                                 pk=self.kwargs['season_pk'])
 
 
 @method_decorator(login_required, name='dispatch')
