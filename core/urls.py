da359a23e42f8d2c85c56427b36dd28ee2a04c98
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-01-08 19:58:21 +0100

    ya va el buscador

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, HistoryView, StatsView
+from .views import *
 
 urlpatterns = [
     # Paths core
-    path("", HomePageView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
+    path('search/', SearchListView.as_view(), name='search'),
     path("history/", HistoryView.as_view(), name="history"),
     path("stats/", StatsView.as_view(), name="stats"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,52 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView, View
 
 from content.models import Movie, Serie
 
+from .forms import SearchForm
 
-class HomePageView(TemplateView):
-    template_name = "core/home.html"
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['movies'] = Movie.objects.all()
-        context['series'] = Serie.objects.all()
+        search_form = SearchForm(self.request.GET)
+        movies = Movie.objects.all()
+        series = Serie.objects.all()
+
+        if search_form.is_valid():
+            query = search_form.cleaned_data.get('query')
+            if query:
+                movies = movies.filter(title__icontains=query)
+                series = series.filter(title__icontains=query)
+
+        context['search_form'] = search_form
+        context['movies'] = movies
+        context['series'] = series
+        return context
+
+
+class SearchListView(ListView):
+    template_name = 'core/home.html'
+    context_object_name = 'search_results'
+
+    def get_queryset(self):
+        form = SearchForm(self.request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            movies = Movie.objects.filter(title__icontains=query)
+            series = Serie.objects.filter(title__icontains=query)
+            return {'movies': movies, 'series': series}
+        else:
+            return {'movies': Movie.objects.none(), 'series': Serie.objects.none()}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = SearchForm(self.request.GET)  # Añade el formulario al contexto
         return context
 
 
