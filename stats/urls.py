478bee23671743284fcf62e865c54f0770fd7910
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-01-12 10:29:40 +0100

    ya está todo hecho en stats

--- stats/urls.py
+++ stats/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('chart/', FavoriteChartView.as_view(), name='favorite_chart'),
-    path('favorites/chart/data/', FavoriteLineChartJSONView.as_view(), name='favorite_chart_data'),
+    path('favorites/chart/', FavoriteChartView.as_view(), name='favorite_chart'),
+    path('favorites/chart/data/', FavoriteChartJSONView.as_view(), name='favorite_chart_data'),
 
-    path('userviewedcontent/chart/', UserViewedContentChartView.as_view(), name='userviewedcontent-chart'),
-    path('userviewedcontent/chart/data/', UserViewedContentLineChartJSONView.as_view(),
-         name='userviewedcontent-chart-data'),
+    path('user_viewed_content/chart/', UserViewedContentChartView.as_view(), name='user_viewed_content_chart'),
+    path('user_viewed_content/chart/data/', UserViewedContentChartJSONView.as_view(),
+         name='user_viewed_content_chart_data'),
+
+    path('series_chart/chart/', FavoriteSeriesChartView.as_view(), name='favorites_series_chart'),
+    path('favorites_series/chart/data/', FavoriteSeriesChartJSONView.as_view(), name='favorites_series_chart_data'),
+
+    path('user_viewed_series_chart/chart/', UserViewedSeriesChartView.as_view(), name='user_viewed_series_chart'),
+    path('user_viewed_series/chart/data/', UserViewedSeriesChartJSONView.as_view(),
+         name='user_viewed_series_chart_data'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.db.models import Count
 from django.utils.decorators import method_decorator
+from django.db.models import Count
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
 
-from content.models import Movie
+from content.models import Movie, Serie
 
 
 def is_admin(user):
@@ -17,7 +17,7 @@ class FavoriteChartView(TemplateView):
 
 
 @method_decorator(user_passes_test(is_admin), name='dispatch')
-class FavoriteLineChartJSONView(BaseLineChartView):
+class FavoriteChartJSONView(BaseLineChartView):
     def get_labels(self):
         """Return labels for the x-axis."""
         labels = Movie.objects.annotate(favorites_count=Count('favorite')).values_list('title', flat=True)
@@ -35,11 +35,11 @@ class FavoriteLineChartJSONView(BaseLineChartView):
 
 @method_decorator(user_passes_test(is_admin), name='dispatch')
 class UserViewedContentChartView(TemplateView):
-    template_name = 'chart.html'
+    template_name = 'stats/chart.html'
 
 
 @method_decorator(user_passes_test(is_admin), name='dispatch')
-class UserViewedContentLineChartJSONView(BaseLineChartView):
+class UserViewedContentChartJSONView(BaseLineChartView):
     def get_labels(self):
         """Return labels for the x-axis."""
         labels = Movie.objects.annotate(view_count=Count('userviewedcontent')).values_list('title', flat=True)
@@ -52,4 +52,49 @@ class UserViewedContentLineChartJSONView(BaseLineChartView):
     def get_data(self):
         """Return dataset to plot."""
         data = Movie.objects.annotate(view_count=Count('userviewedcontent')).values_list('view_count', flat=True)
+        return [list(data)]
+
+
+@method_decorator(user_passes_test(is_admin), name='dispatch')
+class FavoriteSeriesChartView(TemplateView):
+    template_name = 'stats/chart.html'
+
+
+@method_decorator(user_passes_test(is_admin), name='dispatch')
+class FavoriteSeriesChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return labels for the x-axis."""
+        labels = Serie.objects.annotate(favorites_count=Count('favorite__serie')).values_list('title', flat=True)
+        return list(labels)
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Favoritos"]
+
+    def get_data(self):
+        """Return dataset to plot."""
+        data = Serie.objects.annotate(favorites_count=Count('favorite__serie')).values_list('favorites_count',
+                                                                                            flat=True)
+        return [list(data)]
+
+
+@method_decorator(user_passes_test(is_admin), name='dispatch')
+class UserViewedSeriesChartView(TemplateView):
+    template_name = 'stats/chart.html'
+
+
+@method_decorator(user_passes_test(is_admin), name='dispatch')
+class UserViewedSeriesChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return labels for the x-axis."""
+        labels = Serie.objects.annotate(view_count=Count('userviewedcontent')).values_list('title', flat=True)
+        return list(labels)
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Vistas"]
+
+    def get_data(self):
+        """Return dataset to plot."""
+        data = Serie.objects.annotate(view_count=Count('userviewedcontent')).values_list('view_count', flat=True)
         return [list(data)]
